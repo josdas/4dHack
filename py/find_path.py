e5ac7d4c4fa5cfa236145e3ddbddd8d0d6c5d0f0
josdas <josdas@mail.ru>
2017-09-28 23:05:14 +0300

    test and new comment

--- py/find_path.py
+++ py/find_path.py
@@ -1,3 +1,5 @@
+import map_api
+
 
 def find_path(gmap, start, finish, duration, duration_on_foot, money, temp_place, cafe_type, time_cafe):
     """
@@ -7,7 +9,7 @@ def find_path(gmap, start, finish, duration, duration_on_foot, money, temp_place
     :param duration_on_foot: --- float
     :param money: --- float
     :param temp_place: --- [position]
-    :param cafe_type: --- str
+    :param cafe_type: --- str or None
     :param time_cafe: --- float or None
     :return: [positions, ...]
     """
@@ -19,44 +21,109 @@ def find_path(gmap, start, finish, duration, duration_on_foot, money, temp_place
         spent_time = 0
         spent_time_on_foot = 0
 
-        if places[0].position == start:  # если маршрут начинается пешком, то это неплохо
+        # если маршрут начинается пешком, то это неплохо
+        if places[0].position == start:
             score += 10
-        if places[-1].position == finish:  # если маршрут коначается пешком, то это неплохо
+
+        # если маршрут коначается пешком, то это неплохо
+        if places[-1].position == finish:
             score += 10
 
         for place in places:
             place_type = place.info.get('type', None)
-            if place_type == cafe_type and cafe_type is not None:  # если мы хотим сходить в кафе и сходили
+            # если мы хотим сходить в кафе и сходили
+            if place_type == cafe_type and cafe_type is not None:
                 score -= (spent_time - time_cafe) ** 2 * 0.05  #
-                score += 300  # посетил кафе? ты нереально крут
-                spent_time += 20  # в среднем 20 минут сидишь, можно сделать разные времена для разных мест # todo
+                # посетил кафе? ты нереально крут
+                score += 300
+
+                # в среднем 20 минут сидишь, можно сделать разные времена для разных мест # todo
+                spent_time += 20
+
                 spent_money += place.info.get('money', 0)
-                score += place.info.get('rating', 0) * 30  # рейтинг тоже может увеличить очки
+
+                # рейтинг тоже может увеличить очки
+                score += place.info.get('rating', 0) * 30
             if place_type in temp_place:
                 visited.append(place_type)
-                spent_time += 10  # гуляешь в каком-то месте 10 минут
-                spent_time_on_foot += 10  # походили 10 минут
-                score += place.info.get('rating', 0) * 10  # рейтинг влияет чуть хуже
-        if money == 0 and spent_money > 0:  # если мы не хотели тратить, но потратили, то это очень плохо
+
+                # гуляешь в каком-то месте 10 минут
+                spent_time += 10
+
+                # походили 10 минут
+                spent_time_on_foot += 10
+
+                # рейтинг влияет чуть хуже
+                score += place.info.get('rating', 0) * 10
+        # если мы не хотели тратить, но потратили, то это очень плохо
+        if money == 0 and spent_money > 0:
             score -= 100
-        if money - spent_money < 0:  # если мы потратили больше, чем планировали, то тоже плохо
+        # если мы потратили больше, чем планировали, то тоже плохо
+        if money - spent_money < 0:
             score -= 10000
 
-        score += len(set(temp_place) & set(visited)) * 40  # считаем очки за посещенные цели
+        # считаем очки за посещенные цели
+        score += len(set(temp_place) & set(visited)) * 40
 
-        spent_time_on_foot += gmap.get_duration_way(places[1:-1])  # считаем время между точками пешком
+        # считаем время между точками пешком
+        for i in range(len(places) - 1):
+            time_to_next = gmap.get_duration(places[i].position, places[i + 1].position, transit_mode='walking')
+            if time_to_next is not None:
+                spent_time_on_foot += time_to_next
+            else:
+                score -= 100000
 
-        dif_time_on_foot = duration_on_foot - spent_time_on_foot  # считаем штраф за время на ногах
+        # считаем штраф за время на ногах
+        dif_time_on_foot = duration_on_foot - spent_time_on_foot
         if dif_time_on_foot < 0:
             score -= dif_time_on_foot ** 2 * 0.03
         else:
             score -= abs(dif_time_on_foot) ** 3 * 0.005
 
-        dif_time = duration - spent_time  # считаем штраф за время
+        # считаем штраф за время
+        dif_time = duration - spent_time
         if dif_time < 0:
             score -= dif_time ** 2 * 0.03
         else:
             score -= abs(dif_time) ** 3 * 0.005
         return score
 
-    
+    walk_places = []
+    for point in [start, finish]:
+        for place in temp_place:
+            walk_places += gmap.get_places(
+                place,
+                radius=1000000,
+                location=point,
+                min_price=0,
+                max_price=money
+            )
+        if cafe_type is not None:
+            walk_places += gmap.get_places(
+                cafe_type,
+                radius=1000000,
+                location=point,
+                min_price=0,
+                max_price=money
+            )
+    walk_places = list(set(walk_places))
+    print(walk_places)
+
+    return calc_score(walk_places)
+
+if __name__ == '__main__':
+    gmap = map_api.GMap()
+    start = (59.974597, 30.336504)
+    finish = (59.964200, 30.357014)
+    temp = find_path(
+        gmap,
+        start=start,
+        finish=finish,
+        duration=140,
+        duration_on_foot=120,
+        money=300,
+        temp_place=['парк', 'магазин одежды'],
+        cafe_type='кафе',
+        time_cafe=30
+    )
+    print(temp)
