af64e3ddc242f20b9e1649e5af9caf180417c6a3
josdas <josdas@mail.ru>
2017-09-28 18:50:09 +0300

    path

---
+++ py/find_path.py
@@ -0,0 +1,16 @@
+
+def calc(positions):
+
+
+def find_path(start, finish, duration, duration_on_food, money, temp_place, wish_place):
+    """
+    :param start: --- position tuple(float, float)
+    :param finish: --- position tuple(float, float)
+    :param duration: --- float
+    :param duration_on_food: --- float
+    :param money: --- float
+    :param temp_place: --- [position]
+    :param wish_place: --- [(place_name, time), ...]
+    :return: [positions, ...]
+    """
+    
