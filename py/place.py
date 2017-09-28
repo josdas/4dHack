397017c7a6ea9b4bfcf8614a6ecc271da120c99d
josdas <josdas@mail.ru>
2017-09-28 23:04:51 +0300

    hash

--- py/place.py
+++ py/place.py
@@ -2,8 +2,13 @@ class Place:
     def __init__(self, name, position, other_data):
         self.name = name
         self.position = position
-        self.other_data = other_data
+        self.info = other_data
 
     def __str__(self):
         return self.name
 
+    def __eq__(self, other):
+        return self.position == other.position
+
+    def __hash__(self):
+        return 1
